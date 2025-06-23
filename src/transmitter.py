21f4ecc5c714c59cda0d054b104858767bf26eeb
Drew <spacedaoist@gmail.com>
2025-06-23 19:22:42 +0100

    updated socket close condition on transmitter

--- src/transmitter.py
+++ src/transmitter.py
@@ -14,11 +14,19 @@ def transmit_packets(apid=100, interval=1.0, ip=GROUND_IP, port=GROUND_PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     seq_count = 0 
 
-    while True:
-        data = read_pi_data()
-        packet = encode_ccsds_packet(data, apid, seq_count)
-        sock.sendto(packet, (ip, port))
-        hex_display = ":".join(f"{byte:02X}" for byte in packet)
-        print(f"[TX] Packet #{seq_count} → {hex_display}")
-        seq_count = (seq_count + 1) % 16384
-        time.sleep(interval)
+    try:
+
+        while True:
+            data = read_pi_data()
+            packet = encode_ccsds_packet(data, apid, seq_count)
+            sock.sendto(packet, (ip, port))
+            hex_display = ":".join(f"{byte:02X}" for byte in packet)
+            print(f"[TX] Packet #{seq_count} → {hex_display}")
+            seq_count = (seq_count + 1) % 16384
+            time.sleep(interval)
+
+    except KeyboardInterrupt:
+        print("\n[RX] Shutdown requested. Closing socket...", flush=True)
+    finally:
+        sock.close()
+        print("[RX] Socket closed.", flush=True)
