01d141deb03ba4ab2f3ff2a435e3ecef7ee427a7
Andrew Shields <spacedaoist@gmail.com>
2025-06-23 12:13:25 +0100

    updated transmitter to use environment variables for IP and PORT socket binding

--- src/transmitter.py
+++ src/transmitter.py
@@ -2,12 +2,19 @@ import socket
 import time
 from src.csv_reader import read_csv
 from src.packet_encoder import encode_ccsds_packet
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GROUND_IP = os.getenv("GROUND_IP", "127.0.0.1")  # default localhost
+GROUND_PORT = int(os.getenv("GROUND_PORT", 5005))
 
 def transmit_packets(
     apid=100,
     interval=1.0,
-    ip="192.168.1.10",
-    port=5005,
+    ip=GROUND_IP,
+    port=GROUND_PORT,
     filepath="data/tli_telemetry_mvp.csv"
 ):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
