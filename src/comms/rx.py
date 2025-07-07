bd75c116cfe39925dd6577d9724e783ecbb8bd13
SpaceShields <spacedaoist@gmail.com>
2025-07-07 22:54:47 +0100

    updated rx with a minimalist terminal output

--- src/comms/rx.py
+++ src/comms/rx.py
@@ -1,7 +1,7 @@
 import socket
 from src.ccsds.decoder import decode_ccsds_packet
+from src.ccsds.apid import get_subsystem
 import struct
-import pprint
 
 def receive_packets():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,14 +10,13 @@ def receive_packets():
     try:
         while True:
             data, addr = sock.recvfrom(1024)
-            hex_display = ":".join(f"{byte:02X}" for byte in data)
-            print(f"[RX] Received {len(data)} bytes from {addr} -> {hex_display}", flush=True)
+            print(f"[RX] Received {len(data)} bytes from {addr}", flush=True)
             print("[RX] Decoding packet...", flush=True)
             print("==========================", flush=True)
             try:
                 decoded = decode_ccsds_packet(data)
                 if decoded:
-                    print(f"[RX] Decoded packet successfully with payload length {len(data)}.", flush=True)
+                    print(f"[RX] Decoded {get_subsystem(decoded["primary"]["apid"]).upper()} packet #{decoded["primary"]["seq_count"]} successfully with payload length {len(data)}.", flush=True)
             except (ValueError, struct.error) as e:
                 print(f"[RX] Error decoding packet: {e}", flush=True)
                 print("===========================", flush=True)
